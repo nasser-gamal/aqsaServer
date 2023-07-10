acb47993fed87c8679649dd15b569733a8b8ca3d
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-10 18:48:44 +0300

    G

--- src/app.js
+++ src/app.js
@@ -49,6 +49,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
