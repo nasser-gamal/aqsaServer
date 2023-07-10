8a9bc9bad46fd4d8bd3c07c0c3fd6e7c82f5ad66
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-10 18:55:24 +0300

    G

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const Segment = require('./models/segments/segmentsModel.js');
 const Commission = require('./models/commission/commissionModel.js');
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: 'aqsa.alaqsa-pay.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: [
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader('Access-Control-Allow-Origin', 'aqsa.alaqsa-pay.com');
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
