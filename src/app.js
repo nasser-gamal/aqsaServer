99b524ce63a0e3a9ac4866f338a5876d4a01f878
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-10 18:14:48 +0300

    G

--- src/app.js
+++ src/app.js
@@ -44,10 +44,6 @@ app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
   res.setHeader('Content-Type', 'application/json');
   next();
 });
