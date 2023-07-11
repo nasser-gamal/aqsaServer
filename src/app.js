c66258e82d37318f2e8d0a85f6d7428e42dbfdf6
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-11 22:33:51 +0300

    T

--- src/app.js
+++ src/app.js
@@ -34,20 +34,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(morgan('tiny'));
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-//   res.setHeader('Content-Type', 'application/json; charset=utf-8');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next()
+})
 
 User.belongsTo(Role);
 Role.hasMany(User);
