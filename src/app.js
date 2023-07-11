e9469983cdc40a50ee2c1bcf7a171ca93feb6f61
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-11 18:32:32 +0300

    G

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
