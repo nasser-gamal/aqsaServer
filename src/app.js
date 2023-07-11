ffe230d16978c017db330055074c9b3a3ff1398c
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-11 22:37:11 +0300

    T

--- src/app.js
+++ src/app.js
@@ -38,6 +38,19 @@ app.use('/uploads', express.static('uploads'));
 app.use(morgan('tiny'));
 
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next()
+})
 
 
 User.belongsTo(Role);
@@ -108,25 +121,11 @@ UserCommission.hasMany(Commission);
 Commission.belongsTo(UserCommission);
 
 app.use('/api', routes);
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next()
-})
+
 app.use('*', (req, res, next) => {
   next(new ApiError("cann't find this endpoint"));
 });
 app.use(errorHandler);
-
-
 
 const PORT = process.env.PORT || 3000;
 
