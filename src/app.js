229ac374444a7647e193a892efbc5307b0f49da6
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-11 22:35:58 +0300

    T

--- src/app.js
+++ src/app.js
@@ -108,14 +108,12 @@ UserCommission.hasMany(Commission);
 Commission.belongsTo(UserCommission);
 
 app.use('/api', routes);
-
-app.use('*', (req, res, next) => {
-  next(new ApiError("cann't find this endpoint"));
-});
-app.use(errorHandler);
-
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE'
+  )
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
@@ -123,6 +121,12 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next()
 })
+app.use('*', (req, res, next) => {
+  next(new ApiError("cann't find this endpoint"));
+});
+app.use(errorHandler);
+
+
 
 const PORT = process.env.PORT || 3000;
 
