2c22f8f2778a5006f85733db5b11a9bf58f7adc7
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-11 22:35:37 +0300

    T

--- src/app.js
+++ src/app.js
@@ -38,15 +38,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(morgan('tiny'));
 
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next()
-})
+
 
 User.belongsTo(Role);
 Role.hasMany(User);
@@ -121,6 +113,16 @@ app.use('*', (req, res, next) => {
   next(new ApiError("cann't find this endpoint"));
 });
 app.use(errorHandler);
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next()
+})
 
 const PORT = process.env.PORT || 3000;
 
