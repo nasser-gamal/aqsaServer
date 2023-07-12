8b94fef8e61b7f7a5edd1d112e5b9a8552537856
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-12 13:06:55 +0300

    G

--- src/app.js
+++ src/app.js
@@ -39,19 +39,19 @@ app.use(cors(corsOptions));
 app.use(morgan('tiny'));
 
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next()
-})
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PUT, DELETE'
+//   )
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   )
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next()
+// })
 
 
 User.belongsTo(Role);
