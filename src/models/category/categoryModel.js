02d80f1fc4a769e4e1969fc0af28525ea278cd31
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-16 10:22:40 +0300

    G

--- src/models/category/categoryModel.js
+++ src/models/category/categoryModel.js
@@ -1,32 +1,30 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../../config/database.js');
 
-const Category = sequelize.define(
-  'category',
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    imgURL: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    note: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    isActive: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-);
+const Category = sequelize.define('category', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  imgURL: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  note: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  isActive: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+});
 
 module.exports = Category;
