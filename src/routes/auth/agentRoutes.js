17828f810171bda1c018483e4427d21ab520e49d
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-08-11 00:56:05 +0300

    g

--- src/routes/auth/agentRoutes.js
+++ src/routes/auth/agentRoutes.js
@@ -8,7 +8,11 @@ const auth = require('../../middlewares/auth.js');
 const { checkPassword } = require('../../middlewares/checkPassword.js');
 const { checkActive } = require('../../middlewares/checkActive.js');
 
-router.route(links.user.GET_USERS).get(agentControllers.getAllAgents);
+router
+  .route(links.user.GET_USERS)
+  .get(
+    agentControllers.getAllAgents
+  );
 router
   .route(links.user.CREATE_USER)
   .post(
@@ -18,7 +22,7 @@ router
     validate.userValidate,
     validate.agentValidate,
     validate.validateInputs,
-    agentControllers.addAgent~
+    agentControllers.addAgent
   );
 
 router
